4d0745900b95a7c896eaa5f73e58f1a2078a58de
Benjamin Cousin <benou.cousin@gmail.com>
2014-01-09 11:04:20 +0100

    carousel on main skills

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -8,5 +8,17 @@ $(function () {
             $('body').scrollTo('#' + $link.attr('data-scrollToLink'), 500, {offset: {top: -50} });
         });
     });
-    $("[title]").tooltip();
+    $("#owl-example").owlCarousel({
+                                      items: 7, //10 items above 1000px browser width
+                                      itemsDesktop: [1000, 5], //5 items between 1000px and 901px
+                                      itemsDesktopSmall: [900, 4], // betweem 900px and 601px
+                                      itemsTablet: [600, 2], //2 items between 600 and 0
+                                      itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+                                      autoPlay: 3000,
+                                      stopOnHover: true,
+                                      lazyLoad: true,
+                                      afterInit: function () {
+                                          $("[title]").tooltip();
+                                      }
+                                  });
 });
