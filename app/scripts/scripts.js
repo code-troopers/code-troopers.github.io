b5b904133c1583f12202080a11ca99e63555ea31
Benjamin Cousin <benou.cousin@gmail.com>
2014-01-11 17:22:08 +0100

    carousel on project skills

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -8,7 +8,7 @@ $(function () {
             $('body').scrollTo('#' + $link.attr('data-scrollToLink'), 500, {offset: {top: -50} });
         });
     });
-    $("#owl-example").owlCarousel({
+    $("#main-skills-carousel").owlCarousel({
                                       items: 7, //10 items above 1000px browser width
                                       itemsDesktop: [1000, 5], //5 items between 1000px and 901px
                                       itemsDesktopSmall: [900, 4], // betweem 900px and 601px
@@ -21,4 +21,16 @@ $(function () {
                                           $("[title]").tooltip();
                                       }
                                   });
+
+    $(".projects-carousel").owlCarousel({
+                                               items: 4, //10 items above 1000px browser width
+                                               itemsTablet: [600, 2], //2 items between 600 and 0
+                                               itemsMobile: false, // itemsMobile disabled - inherit from itemsTablet option
+                                               autoPlay: 3000,
+                                               stopOnHover: true,
+                                               lazyLoad: true,
+                                               afterInit: function () {
+                                                   $("[title]").tooltip();
+                                               }
+                                           });
 });
