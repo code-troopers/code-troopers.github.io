ceb13af59b49121b086ed4c349fd4a3576e5bf03
Benjamin Cousin <benou.cousin@gmail.com>
2014-01-13 11:47:50 +0100

    fix node version

--- cloudbees-install.sh
+++ cloudbees-install.sh
@@ -10,7 +10,7 @@ Xvfb :1 &
 #cd $1
 
 curl -s -o ./use-node https://repository-cloudbees.forge.cloudbees.com/distributions/ci-addons/node/use-node
-NODE_VERSION=0.11.8 . ./use-node
+NODE_VERSION=0.8.4 . ./use-node
 
 npm cache clean
 
