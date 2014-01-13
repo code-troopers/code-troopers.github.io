a74ece03c07e025e0923a3213d4d05e260c787bf
Benjamin Cousin <benou.cousin@gmail.com>
2014-01-13 11:12:08 +0100

    add cloudbees prerequisites script

---
+++ prerequisites.sh
@@ -0,0 +1,19 @@
+# prerequisites to install on jenkins before each build
+
+export DISPLAY=:1
+Xvfb :1 &
+
+#
+# Fetch node, grunt, bower, npm deps, ruby for compass...
+#
+
+#cd $1
+
+curl -s -o ./use-node https://repository-cloudbees.forge.cloudbees.com/distributions/ci-addons/node/use-node
+NODE_VERSION=0.8.14 . ./use-node
+
+npm install -g grunt-cli bower
+
+npm install
+
+bower install
