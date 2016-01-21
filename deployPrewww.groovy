46a6540edf18b4af4c40ef46c9f537e021d450db
bcousin <benou.cousin@gmail.com>
2016-01-21 15:59:40 +0100

    remove git co form deploy.groovy files

--- deployPrewww.groovy
+++ deployPrewww.groovy
@@ -1,6 +1,5 @@
 stage 'Build image'
 node {
-  git branch: "$BRANCH", credentialsId: 'ca0fa128-d2af-4c9b-9f77-3a2403d46725', url: 'git@github.com:code-troopers/website.git'
   docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
     sh 'cd /src && gulp build --prod --drafts'
     sh 'cd - && tar -czvf source.tgz /src/public'
