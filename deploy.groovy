2b48e5c56e003265e8295527489487401ef8fe4b
Cedric Gatay <cedric@gatay.fr>
2016-01-28 16:07:55 +0100

    fix(build): no node

--- deploy.groovy
+++ deploy.groovy
@@ -1,6 +1,4 @@
-node{
-				docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
-						sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
-						sh 'cd /src && gulp deploy --prod'
-				}
+docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
+		sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
+		sh 'cd /src && gulp deploy --prod'
 }
