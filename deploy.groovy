8aa8b3d666c40e4ffd4754bf341e60716885562c
Cedric Gatay <cedric@gatay.fr>
2016-01-28 16:03:02 +0100

    fix(build): changed load

--- deploy.groovy
+++ deploy.groovy
@@ -1,9 +1,4 @@
-node{
- ws{
-  checkout scm
-  docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
+docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
 		sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
 		sh 'cd /src && gulp deploy --prod'
-  }
- }
 }
