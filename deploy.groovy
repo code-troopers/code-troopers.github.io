673854ccbb53e1e6f27325fb531565ad451f1d3f
Cedric Gatay <cedric@gatay.fr>
2016-01-28 16:04:59 +0100

    fix(build): need to embed in node

--- deploy.groovy
+++ deploy.groovy
@@ -1,4 +1,6 @@
-docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
-		sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
-		sh 'cd /src && gulp deploy --prod'
+node{
+				docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
+						sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
+						sh 'cd /src && gulp deploy --prod'
+				}
 }
