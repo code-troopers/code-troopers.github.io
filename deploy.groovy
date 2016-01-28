ad86959e93ea93b1f8469a3142d8e6c7b4eb3882
Cedric Gatay <cedric@gatay.fr>
2016-01-28 15:37:01 +0100

    fix(deploy.groovy): deploy groovy script

--- deploy.groovy
+++ deploy.groovy
@@ -1,8 +1,4 @@
-node {
-    ws {
-        docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
-            sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
-            sh 'cd /src && gulp deploy --prod'
-        }
-    }
+docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
+				sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
+		sh 'cd /src && gulp deploy --prod'
 }
