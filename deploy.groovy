111fd46a595742177991d68d093d649f391c7d93
Cedric Gatay <cedric@gatay.fr>
2016-01-28 15:51:51 +0100

    feat(deploy.groovy) : try to fix build

--- deploy.groovy
+++ deploy.groovy
@@ -1,4 +1,5 @@
 docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
-				sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
+	  checkout scm
+		sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
 		sh 'cd /src && gulp deploy --prod'
 }
