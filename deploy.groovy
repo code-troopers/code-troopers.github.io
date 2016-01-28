b31a928d2cf4cccaf8a1e9667abfe849295b8a4a
Cedric Gatay <cedric@gatay.fr>
2016-01-28 17:26:22 +0100

    fix(build) : node_modules instead of .git
    Moved node_modules instead of .git

--- deploy.groovy
+++ deploy.groovy
@@ -1,4 +1,4 @@
 docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
 		sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
-		sh 'cp -a .git /src/ && cd /src && gulp deploy --prod'
+		sh 'cp -a ./src/node_modules . && gulp deploy --prod'
 }
