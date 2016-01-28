fec54bd15cdd92b974dfa6f12eacf7ba324e6977
Cedric Gatay <cedric@gatay.fr>
2016-01-28 15:53:17 +0100

    fix(build): need to checkout before building

--- deploy.groovy
+++ deploy.groovy
@@ -1,5 +1,9 @@
-docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
-	  checkout scm
+node{
+ ws{
+  checkout scm
+  docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
 		sh 'git config --global user.email "jenkins@code-troopers.com" && git config --global user.name "Jenkins"'
 		sh 'cd /src && gulp deploy --prod'
+  }
+ }
 }
