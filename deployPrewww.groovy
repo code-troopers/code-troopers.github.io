dcc1c5f8affc8fe5b6e0c04d57180b26d4f52504
Benjamin COUSIN <benou.cousin@gmail.com>
2016-03-03 14:48:18 +0100

    Update deployPrewww.groovy
    remove useless steps

--- deployPrewww.groovy
+++ deployPrewww.groovy
@@ -4,11 +4,5 @@ node {
   docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
     sh 'cd /src && gulp build --prod --drafts'
     sh 'cd - && tar -czvf source.tgz /src/public'
-    stash includes: 'source.tgz', name: 'source'
   }
-}
-stage 'Copy public'
-node('miko') {
-  unstash 'source'
-  sh 'tar -xzvf source.tgz'
 }
