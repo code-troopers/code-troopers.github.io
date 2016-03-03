cca7c523e17b6ea66325ea2ecea83cefae0c196b
Benjamin COUSIN <benou.cousin@gmail.com>
2016-03-03 12:00:17 +0100

    Update deployPrewww.groovy
    build in a new workspace every time

--- deployPrewww.groovy
+++ deployPrewww.groovy
@@ -1,9 +1,11 @@
 stage 'Build image'
 node {
+  ws {
   docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
     sh 'cd /src && gulp build --prod --drafts'
     sh 'cd - && tar -czvf source.tgz /src/public'
     stash includes: 'source.tgz', name: 'source'
+  }
   }
 }
 stage 'Copy public'
