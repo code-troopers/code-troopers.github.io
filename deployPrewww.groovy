a4873af40412d93209dccb9c7dea2a70db5b3402
Benjamin COUSIN <benou.cousin@gmail.com>
2016-03-03 14:43:03 +0100

    Update deployPrewww.groovy
    add checkout scm

--- deployPrewww.groovy
+++ deployPrewww.groovy
@@ -1,5 +1,6 @@
 stage 'Build image'
 node {
+  checkout scm
   docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
     sh 'cd /src && gulp build --prod --drafts'
     sh 'cd - && tar -czvf source.tgz /src/public'
