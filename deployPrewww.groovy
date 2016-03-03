df30aa72c0e5746ae13d44554c2e7d611cd5eeed
Benjamin COUSIN <benou.cousin@gmail.com>
2016-03-03 13:53:47 +0100

    Update deployPrewww.groovy
    remove ws from script

--- deployPrewww.groovy
+++ deployPrewww.groovy
@@ -1,17 +1,13 @@
 stage 'Build image'
 node {
-  ws {
   docker.build('ctwebsite').inside('-v /home/jenkins/.ssh:/home/ct/.ssh') {
     sh 'cd /src && gulp build --prod --drafts'
     sh 'cd - && tar -czvf source.tgz /src/public'
     stash includes: 'source.tgz', name: 'source'
   }
-  }
 }
 stage 'Copy public'
 node('miko') {
-  ws{
   unstash 'source'
   sh 'tar -xzvf source.tgz'
-  }
 }
