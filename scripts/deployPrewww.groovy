247cdd0d64b1ffe7588f28b3277ed87a489e102e
bcousin <benou.cousin@gmail.com>
2016-06-14 21:38:33 +0200

    feat(deploy): fix deployPrewww script

---
+++ scripts/deployPrewww.groovy
@@ -0,0 +1,37 @@
+stage 'prebuild'
+node {
+  git branch: 'feature/hugoDelire', url: 'https://github.com/code-troopers/website'
+  docker.build('website-node').inside(){
+      sh 'rm -rf node_modules && ln -s /usr/src/app/node_modules node_modules'
+      sh 'npm run jenkins.prebuild'
+  }
+}
+
+stage 'build'
+node {
+    docker.image('codetroopers/hugo-asciidoctor').pull()
+  docker.image('codetroopers/hugo-asciidoctor').inside(){
+      sh 'hugo -D -t code-troopers -s site -b https://prewww.code-troopers.com'
+  }
+}
+
+stage 'postbuild'
+node {
+  docker.build('website-node').inside(){
+      sh 'rm -rf node_modules && ln -s /usr/src/app/node_modules node_modules'
+      sh 'npm run jenkins.postbuild'
+      //sh 'npm run jenkins.deploy'
+      stash includes: 'site/public/**', name: 'binary'
+    }
+}
+
+stage 'deploy'
+node {
+    unstash 'binary'
+    sh 'echo "FROM nginx" >> Dockerfile && echo "COPY site/public /usr/share/nginx/html" >> Dockerfile'
+    sh 'docker build -t docker.code-troopers.com/website-prewww:latest .'
+    sh 'docker login -u coruscant -p DtsPtxvthape -e dev@code-troopers.com https://docker.code-troopers.com'
+    sh 'docker push docker.code-troopers.com/website-prewww:latest'
+    sh 'docker rm -fv website-prewww'
+    sh 'docker run -e VIRTUAL_HOST=prewww.code-troopers.com --name website-prewww -d docker.code-troopers.com/website-prewww:latest'
+}
--- scripts/deployPrewww.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "FROM nginx" >> Dockerfile
-echo "COPY site/public /usr/share/nginx/html" >> Dockerfile
-docker build -t docker.code-troopers.com/website-prewww:latest .
-docker login -u coruscant -p DtsPtxvthape -e dev@code-troopers.com https://docker.code-troopers.com
-docker push docker.code-troopers.com/website-prewww:latest
-docker run -e VIRTUAL_HOST=prewww.code-troopers.com --name website-prewww -d docker.code-troopers.com/website-prewww:latest
