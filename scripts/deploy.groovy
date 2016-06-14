50af26d9c33b7bc43a2e2d1a0cdce49132959197
bcousin <benou.cousin@gmail.com>
2016-06-14 21:23:44 +0200

    feat(deploy): add scripts for deploying on prewww

---
+++ scripts/deploy.groovy
@@ -0,0 +1,22 @@
+node {
+  git branch: 'feature/hugoDelire', url: 'https://github.com/code-troopers/website'
+  docker.build('website-node').inside(){
+      sh 'rm -rf node_modules && ln -s /usr/src/app/node_modules node_modules'
+      sh 'npm run jenkins.prebuild'
+  }
+}
+
+node {
+    docker.image('codetroopers/hugo-asciidoctor').pull()
+  docker.image('codetroopers/hugo-asciidoctor').inside(){
+      sh 'hugo -t code-troopers -s site -b https://code-troopers.com'
+  }
+}
+
+node {
+  docker.build('website-node').inside(){
+      sh 'rm -rf node_modules && ln -s /usr/src/app/node_modules node_modules'
+      sh 'npm run jenkins.postbuild'
+      //sh 'npm run jenkins.deploy'
+    }
+}
