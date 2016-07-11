2e580fd1b8ad92f8e279ec1572d245278cd7419f
bcousin <benou.cousin@gmail.com>
2016-07-11 16:43:55 +0200

    feat(deploy): checkout git repo with git url

--- scripts/deploy.groovy
+++ scripts/deploy.groovy
@@ -1,5 +1,5 @@
 node {
-  git branch: 'master', url: 'https://github.com/code-troopers/website.git'
+  git credentialsId: 'ca0fa128-d2af-4c9b-9f77-3a2403d46725', url: 'git@github.com:code-troopers/website.git'
   docker.build('website-node').inside(){
       sh 'rm -rf node_modules && ln -s /usr/src/app/node_modules node_modules'
       sh 'npm run jenkins.prebuild'
@@ -16,7 +16,6 @@ node {
 node {
   docker.build('website-node').inside('-v /var/jenkins_home/.ssh:/root/.ssh') {
       sh 'rm -rf node_modules && ln -s /usr/src/app/node_modules node_modules'
-      sh 'git remote set-url origin git@github.com:code-troopers/website.git'
       sh 'npm run jenkins.postbuild'
       sh 'npm run jenkins.deploy'
     }
