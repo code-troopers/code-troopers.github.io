b744708ea6c328689d5085d96420ed391e50aede
Matthieu Bollot <m.bollot@code-troopers.com>
2017-09-28 23:27:13 +0200

    fix(docker): new version of docker removed --email

--- scripts/deployPrewww.groovy
+++ scripts/deployPrewww.groovy
@@ -30,7 +30,7 @@ node {
     unstash 'binary'
     sh 'echo "FROM nginx" >> Dockerfile && echo "COPY site/public /usr/share/nginx/html" >> Dockerfile'
     sh 'docker build -t docker.code-troopers.com/website-prewww:latest .'
-    sh 'docker login -u coruscant -p DtsPtxvthape -e dev@code-troopers.com https://docker.code-troopers.com'
+    sh 'docker login -u coruscant -p DtsPtxvthape https://docker.code-troopers.com'
     sh 'docker push docker.code-troopers.com/website-prewww:latest'
     sh 'docker rm -fv website-prewww'
     sh 'docker run -e VIRTUAL_HOST=prewww.code-troopers.com --name website-prewww -d docker.code-troopers.com/website-prewww:latest'
