3840a0049fa9d4aba7120123783eb955485ebe40
Matthieu Bollot <m.bollot@code-troopers.com>
2024-03-27 08:53:32 +0100

    refactor: 💡 refactor to avoid else

--- site/static/js/april.js
+++ site/static/js/april.js
@@ -1,34 +1,34 @@
 
 (async function($) {
-    const d = new Date();
-    if (d.getMonth() === 3 && d.getDate() === 1) {
-        $("#april").html("<a href='#' id='disable-spotlight' class='btn dark'>Désactiver le mode sombre</a><div id='spotlight'></div>");
-    } else {
-        return;
-    }
+  const d = new Date();
+  if (d.getMonth() !== 3 || d.getDate() !== 1) {
+    return;
+  }
 
-const spotlightEl = $("#spotlight");
+  $("#april").html("<a href='#' id='disable-spotlight' class='btn dark'>Désactiver le mode sombre</a><div id='spotlight'></div>");
 
-function handleMouseMove(event) {
-    const { clientX, clientY } = event;
-    
+  const spotlightEl = $("#spotlight");
+
+  function handleMouseMove(event) {
+    const {clientX, clientY} = event;
+
     spotlightEl[0].style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00000000 10px, #000000FF 100px)`;
-}
+  }
 
-$("body").on("mousemove", handleMouseMove)
+  $("body").on("mousemove", handleMouseMove);
 
-$("#disable-spotlight").on("click", () => {
+  $("#disable-spotlight").on("click", () => {
     disableApril();
     localStorage.setItem("disable-april", "true");
-})
+  });
 
-if (localStorage.getItem("disable-april") === "true") {
+  if (localStorage.getItem("disable-april") === "true") {
     disableApril();
-}
+  }
 
-    function disableApril() {
-        $("#spotlight")[0].style.display = "none";
-        $("#disable-spotlight")[0].style.display = "none";
-        $("body").off("mousemove", handleMouseMove);
-    }
-})(jQuery)
+  function disableApril() {
+    $("#spotlight")[0].style.display = "none";
+    $("#disable-spotlight")[0].style.display = "none";
+    $("body").off("mousemove", handleMouseMove);
+  }
+})(jQuery);
