dc5e4c7054a72cfd8744504db9d147769cca46a4
Matthieu Bollot <m.bollot@code-troopers.com>
2025-10-16 15:41:10 +0200

    chore: bump some deps

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,20 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const AssetsPlugin = require("assets-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const webpack = require("webpack");
+
+class RemoveNodeSchemePlugin {
+  apply(compiler) {
+    compiler.hooks.normalModuleFactory.tap("RemoveNodeSchemePlugin", (nmf) => {
+      nmf.hooks.beforeResolve.tap("RemoveNodeSchemePlugin", (data) => {
+        if (!data || !data.request) return;
+        if (data.request.startsWith("node:")) {
+          data.request = data.request.replace(/^node:/, "");
+        }
+      });
+    });
+  }
+}
 
 module.exports = {
   entry: {
@@ -14,93 +28,100 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     publicPath: "",
+    clean: true,
   },
 
   module: {
     rules: [
       {
         test: /\.m?js$/,
-        resolve: { fullySpecified: false }, // pour les .mjs
+        resolve: { fullySpecified: false },
       },
       {
-        test: /\.((png)|(eot)|(woff)|(woff2)|(ttf)|(svg)|(gif))(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(png|eot|woff2?|ttf|svg|gif)(\?v=\d+\.\d+\.\d+)?$/,
         type: "asset/resource",
       },
       {
-        loader: "babel-loader",
-        test: /\.js?$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        options: { cacheDirectory: true },
+        use: {
+          loader: "babel-loader",
+          options: { cacheDirectory: true },
+        },
       },
       {
         test: /\.(sa|sc|c)ss$/,
         exclude: /node_modules/,
         use: [
-          "style-loader",
+          MiniCssExtractPlugin.loader,
           {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              esModule: false,
-            },
+            loader: "css-loader",
+            options: { sourceMap: true },
           },
-          "css-loader",
           {
             loader: "resolve-url-loader",
+            options: { sourceMap: true },
           },
           {
             loader: "postcss-loader",
-            options: {
-              sourceMap: true,
-            },
+            options: { sourceMap: true },
           },
           {
             loader: "sass-loader",
             options: {
               sourceMap: true,
-              sassOptions: {
-                outputStyle: "expanded",
-              },
+              sassOptions: { outputStyle: "expanded" },
             },
           },
         ],
       },
     ],
   },
+
   resolve: {
+    extensions: [".js", ".scss"],
     alias: {
-      "node:url": require.resolve("url/"),
+      "@": path.resolve(__dirname, "src/"),
     },
     fallback: {
       buffer: require.resolve("buffer/"),
       process: require.resolve("process/browser"),
       url: require.resolve("url/"),
       path: require.resolve("path-browserify"),
+      stream: require.resolve("stream-browserify"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
     },
   },
 
   plugins: [
+    new NodePolyfillPlugin(),
+    new RemoveNodeSchemePlugin(),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^clean-stack$/,
+    }),
+
     new AssetsPlugin({
       filename: "webpack.json",
       path: path.join(process.cwd(), "site/data"),
       prettyPrint: true,
     }),
+
     new CopyWebpackPlugin({
       patterns: [
-        {
-          from: "./src/fonts/",
-          to: "fonts/",
-        },
-        {
-          from: "./site/data/cloud.json",
-          to: "data/cloud.json",
-        },
+        { from: "./src/fonts/", to: "fonts/" },
+        { from: "./site/data/cloud.json", to: "data/cloud.json" },
       ],
     }),
+
     new HtmlWebpackPlugin({
       filename: "admin/index.html",
       template: "src/cms.html",
       inject: true,
     }),
-    new NodePolyfillPlugin(),
+
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
   ],
 };
