d6be9bc5fcc7899e752bfb52d710272a3534875c
Florian Chauveau <f.chauveau@code-troopers.com>
2025-11-13 14:27:37 +0100

    Feature/navig (#132)
    * chore: clean css
    
    * feat(navig): copy/past from Nico code
    
    * feat(navig): extract iframe
    
    * feat(navig): small improvements
    
    * feat(navig): css fixes
    
    * chore: bump decap-cms
    
    * chore: bump some packages
    
    * wip
    
    * chore: bump some deps
    
    * chore: set max width for blog snippet
    
    * feat(style): update colors for halloween
    
    * fix(blog): now summary have tags by default
    
    * chore: fix typo
    
    * chore: fix problem with merge
    
    * feat(navig): update page content
    
    * feat(navig): images lost + content
    
    * feat(footer): update social link
    
    * feat(navig): update highlight section
    
    * chore(): gitignore
    
    * chore: bump node in nvmrc
    
    * feat(navig) typo
    
    * feat(navig): update image
    
    * feat(style): fix menu on mobile
    
    * feat(style): fix css on tablet
    
    * feat(style): fix buttons outside #contact css
    
    * feat(style): fix social buttons
    
    * feat(style): fix media queries due to hugo/sass update
    
    * fix(style): fix style on mobile / tablet on page navig
    
    * feat(halloween): revert
    
    * feat(tips): remove for now
    
    * feat(navig): update alignement
    
    * feat(navig): increase first pic size
    
    * feat(navig): improve first pic size
    
    * feat(navig): css mobile improvement
    
    ---------
    
    Co-authored-by: Matthieu Bollot <m.bollot@code-troopers.com>

--- src/index.js
+++ src/index.js
@@ -1,45 +1,52 @@
 // JS Goes here - ES6 supported
+import process from "process";
+import { Buffer } from "buffer";
+
+window.process = process;
+window.Buffer = Buffer;
 
 import "./css/main.scss";
 import "../node_modules/lightbox2/dist/js/lightbox-plus-jquery";
 
-
 const hljs = require("../node_modules/highlight.js/lib/common");
 hljs.highlightAll();
 
-
 function updateMenu() {
-    var scrollPosition = window.scrollY;
+  var scrollPosition = window.scrollY;
 
-    var selectedSectionId = "brand";
-    for (var section of document.querySelectorAll("section")) {
-        var sectionTop = section.offsetTop - 200;
-        var sectionBottom = sectionTop + section.offsetHeight;
-        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-            selectedSectionId = section.id;
-            break;
-        }
+  var selectedSectionId = "brand";
+  for (var section of document.querySelectorAll("section")) {
+    var sectionTop = section.offsetTop - 200;
+    var sectionBottom = sectionTop + section.offsetHeight;
+    if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+      selectedSectionId = section.id;
+      break;
     }
+  }
 
-    var links = document.querySelectorAll("nav a");
-    for (var link of links) {
-        link.classList.remove("active");
-        if (selectedSectionId !== "" && link.id.includes(selectedSectionId)) {
-            link.classList.add("active");
-        }
+  var links = document.querySelectorAll("nav a");
+  for (var link of links) {
+    link.classList.remove("active");
+    if (selectedSectionId !== "" && link.id.includes(selectedSectionId)) {
+      link.classList.add("active");
     }
+  }
 }
 window.addEventListener("scroll", updateMenu);
 window.addEventListener("load", updateMenu);
 
 // Matomo
-var _paq = window._paq = window._paq || [];
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
+var _paq = (window._paq = window._paq || []);
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
 (function () {
-    var u = "//matomo.chapi.to/";
-    _paq.push(['setTrackerUrl', u + 'matomo.php']);
-    _paq.push(['setSiteId', '69']);
-    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+  var u = "//matomo.chapi.to/";
+  _paq.push(["setTrackerUrl", u + "matomo.php"]);
+  _paq.push(["setSiteId", "69"]);
+  var d = document,
+    g = d.createElement("script"),
+    s = d.getElementsByTagName("script")[0];
+  g.async = true;
+  g.src = u + "matomo.js";
+  s.parentNode.insertBefore(g, s);
 })();
