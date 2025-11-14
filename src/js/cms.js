9159ad1b53cbc7180d0778bf041a8362fb2a57a9
Matthieu Bollot <m.bollot@code-troopers.com>
2025-11-14 11:25:56 +0100

    chore: replace timeFormat with new syntax time_format

--- src/js/cms.js
+++ src/js/cms.js
@@ -14,74 +14,72 @@ CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("footer", FooterPreview);
 
-
 CMS.registerEditorComponent({
-    // Internal id of the component
-    id: "lightbox-image",
-    // Visible label
-    label: "Lightbox Image",
-    // Fields the user need to fill out when adding an instance of the component
-    fields: [
-        {
-            name: "title",
-            label: "Title",
-            widget: "string"
-        },
-        {
-            name: "group",
-            label: "Group",
-            widget: "string"
-        },
-        {
-            name: "image",
-            label: "Image",
-            widget: "image"
-        },
-    ],
-    // Regex pattern used to search for instances of this block in the markdown document.
-    // Patterns are run in a multiline environment (against the entire markdown document),
-    // and so generally should make use of the multiline flag (`m`). If you need to capture
-    // newlines in your capturing groups, you can either use something like
-    // `([\S\s]*)`, or you can additionally enable the "dot all" flag (`s`),
-    // which will cause `(.*)` to match newlines as well.
-    //
-    // Additionally, it's recommended that you use non-greedy capturing groups (e.g.
-    // `(.*?)` vs `(.*)`), especially if matching against newline characters.
-    pattern: /^-![(.*)]\((.*?) (.*?)\)$/ms,
-    // Given a RegExp Match object
-    // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match#return_value),
-    // return an object with one property for each field defined in `fields`.
-    //
-    // This is used to populate the custom widget in the markdown editor in the CMS.
-    fromBlock: function (match) {
-        return {
-            title: match[1],
-            image: match[2],
-            group: match[3]
-        };
+  // Internal id of the component
+  id: "lightbox-image",
+  // Visible label
+  label: "Lightbox Image",
+  // Fields the user need to fill out when adding an instance of the component
+  fields: [
+    {
+      name: "title",
+      label: "Title",
+      widget: "string",
     },
-    // Given an object with one property for each field defined in `fields`,
-    // return the string you wish to be inserted into your markdown.
-    //
-    // This is used to serialize the data from the custom widget to the
-    // markdown document
-    toBlock: function (data) {
-        return `
+    {
+      name: "group",
+      label: "Group",
+      widget: "string",
+    },
+    {
+      name: "image",
+      label: "Image",
+      widget: "image",
+    },
+  ],
+  // Regex pattern used to search for instances of this block in the markdown document.
+  // Patterns are run in a multiline environment (against the entire markdown document),
+  // and so generally should make use of the multiline flag (`m`). If you need to capture
+  // newlines in your capturing groups, you can either use something like
+  // `([\S\s]*)`, or you can additionally enable the "dot all" flag (`s`),
+  // which will cause `(.*)` to match newlines as well.
+  //
+  // Additionally, it's recommended that you use non-greedy capturing groups (e.g.
+  // `(.*?)` vs `(.*)`), especially if matching against newline characters.
+  pattern: /^-![(.*)]\((.*?) (.*?)\)$/ms,
+  // Given a RegExp Match object
+  // (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match#return_value),
+  // return an object with one property for each field defined in `fields`.
+  //
+  // This is used to populate the custom widget in the markdown editor in the CMS.
+  fromBlock: function (match) {
+    return {
+      title: match[1],
+      image: match[2],
+      group: match[3],
+    };
+  },
+  // Given an object with one property for each field defined in `fields`,
+  // return the string you wish to be inserted into your markdown.
+  //
+  // This is used to serialize the data from the custom widget to the
+  // markdown document
+  toBlock: function (data) {
+    return `
 <a style="display: inline" href="${data.image}" data-lightbox="${data.group}" title="${data.title}">
   <img class="medium" src="${data.image}" alt="${data.title}">
 </a>
 `;
-    },
-    // Preview output for this component. Can either be a string or a React component
-    // (component gives better render performance)
-    toPreview: function (data) {
-        return `
+  },
+  // Preview output for this component. Can either be a string or a React component
+  // (component gives better render performance)
+  toPreview: function (data) {
+    return `
 <a style="display: inline" href="${data.image}" data-lightbox="${data.group}" title="${data.title}">
   <img class="medium" src="${data.image}" alt="${data.title}">
 </a>
 `;
-    }
+  },
 });
-
 
 CMS.init();
