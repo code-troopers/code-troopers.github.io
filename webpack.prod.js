97f8ca5d7309ff794c697ede206caf5fa23088e3
Matthieu Bollot <m.bollot@code-troopers.com>
2025-11-17 10:13:45 +0100

    feat(cms): fix production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,4 @@
-const {merge} = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const path = require("path");
 
 const TerserPlugin = require("terser-webpack-plugin");
@@ -11,9 +11,9 @@ module.exports = merge(common, {
   mode: "production",
 
   output: {
-    filename: "[name].[hash:5].js",
-    chunkFilename: "[id].[hash:5].css",
-    path: path.resolve(__dirname, "dist")
+    filename: "[name].[fullhash:8].js",
+    chunkFilename: "[name].[fullhash:8].js",
+    path: path.resolve(__dirname, "dist"),
   },
 
   optimization: {
@@ -25,11 +25,13 @@ module.exports = merge(common, {
         },
         exclude: /\/node_modules\//,
       }),
-      new MiniCssExtractPlugin({
-        filename: "[name].[hash:5].css",
-        chunkFilename: "[id].[hash:5].css"
-      }),
       new CssMinimizerPlugin(),
-    ]
-  }
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[fullhash:8].css",
+      chunkFilename: "[name].[fullhash:8].css",
+    }),
+  ],
 });
